032bb1273aed85b92598abd7ef655bf85a9c2b21
theoranelletti <111407220+theoranelletti@users.noreply.github.com>
2022-09-07 11:22:08 -0700

    Updating assignments/number-functions/code.js

--- assignments/number-functions/code.js
+++ assignments/number-functions/code.js
@@ -29,7 +29,7 @@ const manhattanDistance = (x1,y1,x2,y2) => {
 const euclideanDistanc = (x1,y1,x2,y2) => {
   return Math.sqrt(Math.abs(x1-x2)) +  Math.sqrt(Math.abs(y1-y2));
 }
-double distance (Point other) {
+const euclideanDistance double distance (Point other) {
     double deltaX = ycoord - other.ycoord;
     double deltaY = xcoord - other.xcoord;
     double result = Math.sqrt(deltaX*deltaX + deltaY*deltaY);
