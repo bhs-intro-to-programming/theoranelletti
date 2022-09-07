fab49184b24260f55f87c5f5a13e6000e8143bec
theoranelletti <111407220+theoranelletti@users.noreply.github.com>
2022-09-07 15:05:56 -0700

    Updating assignments/boolean-birds/code.js

--- assignments/boolean-birds/code.js
+++ assignments/boolean-birds/code.js
@@ -1,24 +1,19 @@
 /*
  * Important facts about the birds of Booleana.
- *
+ * 
  * Coloration and markings:
- *
+ * 
  *   - Flobby Birds and Bloggy Birds are red.
  *   - Flibble Birds and Globby Birds are not red.
  *   - Flobby Birds and Flibble Birds are spotted.
  *   - Bloggy Birds and Globby Birds are not spotted.
- *
  * Diet:
- *
+ * 
  *   - Flobby Birds eat fish, nuts, and worms.
  *   - Bloggy Birds eat mice, nuts, and worms.
  *   - Flibble Birds eat fish, mice, and worms.
  *   - Globby Birds eat fish, mice, and nuts.
- *
  */
-x1 = isRed
-let let x2 = isSpotted
-const isFloppyBird  = (x1, x2) => {
-  (x1);
-  (x2);
+const isFlobbyBird = (red,spotted) => {
+
 }
