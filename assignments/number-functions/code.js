3b544dfbd179d46c962bf8fb631a94f38830d39a
theoranelletti <111407220+theoranelletti@users.noreply.github.com>
2022-09-07 14:40:37 -0700

    Updating assignments/number-functions/code.js

--- assignments/number-functions/code.js
+++ assignments/number-functions/code.js
@@ -26,5 +26,5 @@ const manhattanDistance = (x1,y1,x2,y2) => {
   return Math.abs(x1-x2) + Math.abs(y1-y2);
 }
 const euclideanDistance = (x1,y1,x2,y2) => {
-  return Math.sqrt(Math.abs(x1-x2) +  Math.sqrt(Math.abs(y1-y2));
+  return Math.sqrt(Math.abs(x1-x2) +  (Math.abs(y1-y2);
 }
