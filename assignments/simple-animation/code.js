5303db4cd96784f56118d87f821402fa99d22d5d
theoranelletti <111407220+theoranelletti@users.noreply.github.com>
2022-09-09 15:02:01 -0700

    Updating assignments/simple-animation/code.js

--- assignments/simple-animation/code.js
+++ assignments/simple-animation/code.js
@@ -3,6 +3,6 @@ const drawFrame = (time) => {
   drawFilledCircle((time / 2) % width, height / 7, 25, 'grey');
   drawFilledCircle((time / 120) % width, height / 2, 25, 'orange');
   drawFilledCircle((time / 7200) % width, height / 1.2, 25, 'blue');
-  const drawFilledTriangle(10, 10, 10, height/2, width/2, height/3, 'blue');
+  const drawFilledTriangle (10, 10, 10, height/2, width/2, height/3, 'blue');
 };
   animate(drawFrame);
