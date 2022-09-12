cd1e117110efe21b5eb61c990050e01f7b135c0b
theoranelletti <111407220+theoranelletti@users.noreply.github.com>
2022-09-12 15:28:53 -0700

    Updating assignments/pig-latin/code.js

--- assignments/pig-latin/code.js
+++ assignments/pig-latin/code.js
@@ -31,3 +31,12 @@
 //
 // Feel free to play around in the REPL to make sure you understand how this
 // operator works.
+
+const pigLatin = (w) => {
+  var l= (w.search(/[aeiou]/))
+  return w.substring(l,w.length) + w.substring(0,l) + 'ay'
+}
+const advancedPigLatin = (w) => {
+  var l= (w.search(/[aeiou]/))
+  return l === 0 ? w+"way" : w.substring(l,w.length) + w.substring(0,l) + 'ay'
+}
