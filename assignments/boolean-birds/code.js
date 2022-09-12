e03f37a595e4d4b01d194cb661e87d6b96679b75
theoranelletti <111407220+theoranelletti@users.noreply.github.com>
2022-09-12 15:07:00 -0700

    Updating assignments/boolean-birds/code.js

--- assignments/boolean-birds/code.js
+++ assignments/boolean-birds/code.js
@@ -14,39 +14,39 @@
  *   - Flibble Birds eat fish, mice, and worms.
  *   - Globby Birds eat fish, mice, and nuts.
  */
-const isFlobbyBird = (red,spotted) => {
+const isFlobbyBird = (red, spotted) => {
   return red && spotted
 }
-const isBloggyBird = (red,spotted) => {
-  return red &&! spotted
+const isBloggyBird = (red, spotted) => {
+  return red && !spotted
 }
-const isFlibbleBird = (red,spotted) => {
-  return ! red && spotted
+const isFlibbleBird = (red, spotted) => {
+  return !red && spotted
 }
-const isGlobbyBird = (red,spotted) => {
-  return !red && ! spotted
+const isGlobbyBird = (red, spotted) => {
+  return !red && !spotted
 }
-const eatsWorms = (red,spotted) => {
+const eatsWorms = (red, spotted) => {
   return red || spotted
 }
-const eatsNuts = (red,spotted) => {
-  return red ||! spotted
+const eatsNuts = (red, spotted) => {
+  return red || !spotted
 }
-const eatsFish = (red,spotted) => {
-  return ! red || spotted
+const eatsFish = (red, spotted) => {
+  return !red || spotted
 }
-const eatsMice = (red,spotted) => {
-  return ! red ||! spotted
+const eatsMice = (red, spotted) => {
+  return !red || !spotted
 }
 const isRed = (red) => {
-  return red==='Flobby'||red==='Bloggy'
+  return red === 'Flobby' || red === 'Bloggy'
 }
 const isSpotted = (spot) => {
-  return spot==='Flobby'||spot==='Flibble'
+  return spot === 'Flobby' || spot === 'Flibble'
 }
 const isNotRed = (notred) => {
-  return notred==='Flibble'||notred==='Globby'
+  return notred === 'Flibble' || notred === 'Globby'
 }
 const isNotSpotted = (notspot) => {
-  return notspot==='Bloggy'||notspot==='Globby'
+  return notspot === 'Bloggy' || notspot === 'Globby'
 }
