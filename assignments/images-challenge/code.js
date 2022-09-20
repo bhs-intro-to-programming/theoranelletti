231799bb4f37ce6ed3ff972ea4c39811c77f3b4f
theoranelletti <111407220+theoranelletti@users.noreply.github.com>
2022-09-20 15:09:48 -0700

    Updating assignments/images-challenge/code.js

--- assignments/images-challenge/code.js
+++ assignments/images-challenge/code.js
@@ -27,7 +27,7 @@
 const fillwithcircles = () => {
   for (let x = 0; x < 32; x++)
     if (x > length) {
-      drawFilledCircle(x * 15.5, height / 2, 8, 'beige')
+      drawFilledCircle(x * 15.5, height / 2, 8, 'purple')
     }
 }
 fillwithcircles();
