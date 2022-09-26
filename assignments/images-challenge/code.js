1863a75f966dda0c2eb7921b63bcdbf3549641c5
theoranelletti <111407220+theoranelletti@users.noreply.github.com>
2022-09-26 15:27:20 -0700

    Updating assignments/images-challenge/code.js

--- assignments/images-challenge/code.js
+++ assignments/images-challenge/code.js
@@ -24,7 +24,7 @@
  *  clear()
  */
 
-const numCircles = Math.Floor * (width / d) => {
+const numCircles = Math.Floor * (width / d) {
   const fillwithCircles = (r);
     const d = r * 2
   for (let x = 1; x < numCircles; x++) {
