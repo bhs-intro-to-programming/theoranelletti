15f089cf6af8a041fe3e4a2f5db877ea2d988b16
theoranelletti <111407220+theoranelletti@users.noreply.github.com>
2022-09-29 15:12:38 -0700

    Updating assignments/images-challenge/code.js

--- assignments/images-challenge/code.js
+++ assignments/images-challenge/code.js
@@ -24,9 +24,9 @@
  *  clear()
  */
 
+const d = r * 2
 const n = Math.floor(width / d);
 const fillwithCircles = (r) => {
-  const d = r * 2
   for (let x = 1; x < n; x++)
     if (x > length) {
       drawFilledCircle(x * 40, height / 2, 20, 'blue')
