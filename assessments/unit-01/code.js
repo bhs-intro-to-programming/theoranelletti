45e56363afc36faed8313303457e9cb81dba7d33
theoranelletti <111407220+theoranelletti@users.noreply.github.com>
2022-09-30 14:47:33 -0700

    Updating assessments/unit-01/code.js

--- assessments/unit-01/code.js
+++ assessments/unit-01/code.js
@@ -4,4 +4,9 @@ const emit = (a, b, product) => {
   console.log(`${a} times ${b} is ${product}`);
 };
 
-// Write your code here ...
+const canSleepIn ('weekend', 'vacation') => {
+  vacation && weekend &&! weekday
+}
+const cantSleepIn ('weekday') => {
+  !vacation &&! weeked && weekday
+}
