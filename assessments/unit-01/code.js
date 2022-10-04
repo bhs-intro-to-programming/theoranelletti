7702976c6f77e456e1512e0ade2f398e4bdfa431
theoranelletti <111407220+theoranelletti@users.noreply.github.com>
2022-10-04 14:49:18 -0700

    Updating assessments/unit-01/code.js

--- assessments/unit-01/code.js
+++ assessments/unit-01/code.js
@@ -1,9 +1,12 @@
 // You will use this function in one of the problems. You don't need to change
 // it or even worry about how exactly it works.
-const emit = (a, b, product) => {
-  console.log(`${a} times ${b} is ${product}`);
-};
-
+//const emit = (a, b, product) => {
+ // console.log(`${a} times ${b} is ${product}`);
+//};
 const averageWeight = (weight, items) => {
   return weight / items;
 };
+
+//const hypotenuse = (a, b) => {
+ // return Math.sqrt(a ** 2 + b ** 2);
+//};
