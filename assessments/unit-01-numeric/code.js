bdf3c5a5e2cbb4e8984308a7701b344f75f11315
theoranelletti <111407220+theoranelletti@users.noreply.github.com>
2022-10-07 15:15:42 -0700

    Updating assessments/unit-01-numeric/code.js

--- assessments/unit-01-numeric/code.js
+++ assessments/unit-01-numeric/code.js
@@ -8,6 +8,42 @@ const G = 6.6743e-11;
 // reasonable argument list, yet more credit for a working function, and full
 // credit for a clear and simple working function.
 
+const itemsLeftOver = (numpeople, numitems) => {
+  return
+};
+
 const areaOfCircles = (r) => {
   return Math.PI(r ** 2);
+};
+
+const volumeOfCube (length) => {
+  return length ** 3;
+};
+
+const populationGrowth = (current, growth) => {
+  return currentsize + (current / growth);
+};
+
+const earnedRunAverage = (earnedruns, innings) => {
+  return (earnedruns / innings) * 9;
+};
+
+const valueOfJewels = (diamonds, emeralds, onediamondingold, oneemeraldingold) => {
+  return
+};
+
+const payWitOvertime = () => {
+  return
+};
+
+const firstClassPostage = () => {
+  return
+};
+
+const wieghtOnJupiter = () => {
+  return
+};
+
+const gravity = () => {
+  return
 };
