f6643e75eeeca19b2ace22f37bc95f1eba146aed
theoranelletti <111407220+theoranelletti@users.noreply.github.com>
2022-10-20 14:48:25 -0700

    Updating assignments/array-functions/code.js

--- assignments/array-functions/code.js
+++ assignments/array-functions/code.js
@@ -1,4 +1,9 @@
-const countTens = () => {
-  let
-  return
+const countTens = (array) => {
+  let num = 0
+  for (const element of array) {
+    if (element === 10) {
+      num++
+    }
+  }
+  return num
 };
