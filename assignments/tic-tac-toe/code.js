8224df32db1d732aca6163a27da1bd330698f2be
theoranelletti <111407220+theoranelletti@users.noreply.github.com>
2022-11-01 15:18:17 -0700

    Updating assignments/tic-tac-toe/code.js

--- assignments/tic-tac-toe/code.js
+++ assignments/tic-tac-toe/code.js
@@ -6,7 +6,7 @@
 // weeks but for now you can just adapt this code.
 
 registerOnclick((x, y) => {
-  drawText('145678088', x - 50, y + 50, 'black', Math.min(600, 600) * 0.3);
+  drawText('8888888888888888888888888', x - 50, y + 50, 'black', Math.min(600, 600) * 0.3);
 });
 
 const drawBoard = () => {
