a6ae266a3944b3c60ccf07c2ce6c1f32657d1fce
theoranelletti <111407220+theoranelletti@users.noreply.github.com>
2022-11-07 15:09:44 -0800

    Updating assignments/object-functions/code.js

--- assignments/object-functions/code.js
+++ assignments/object-functions/code.js
@@ -2,8 +2,6 @@ const getX = (o) => {
   return o.x
 }
 
-const point = (o1, o2) => {
-  const o1 = x
-  const o2 = y
-  return o1.x + o2.y
+const point = (x, y) => {
+  return {x : 'x' , y : 'y'}
 }
