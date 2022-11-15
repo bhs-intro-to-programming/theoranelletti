cc6349577fb4448fd4965d450e7773a0178dee8f
theoranelletti <111407220+theoranelletti@users.noreply.github.com>
2022-11-15 14:57:46 -0800

    Updating assignments/chess-board/code.js

--- assignments/chess-board/code.js
+++ assignments/chess-board/code.js
@@ -13,3 +13,28 @@ const BLACK_PAWN   = '♟';
 
 // Example of drawing one of the pieces
 drawText(WHITE_KING, width/2, height/2, 'black', 64);
+
+let turn = "X";
+registerOnclick((x, y) => {
+
+  const rowHeight = height / 3
+  const box_y = Math.floor(y / rowHeight)
+
+  const rowWidth = width / 3
+  const box_x = Math.floor(x / rowWidth)
+  drawText(turn, box_x * (width / 3) - 75 + width / 6, box_y * (height / 3) + 75 + height / 6, 'black', 180);
+  turn = turn === "X" ? "O" : "X";
+});
+
+const drawBoard = () => {
+  for (let i = 0; i < 2; i++) {
+    const x = (i + 1) * width / 3
+    drawLine(x, 0, x, height, 'black', 3)
+  }
+  for (let i = 0; i < 2; i++) {
+    const x = (i + 1) * height / 3
+    drawLine(0, x, width, x, 'black', 3)
+  }
+}
+
+drawBoard()
