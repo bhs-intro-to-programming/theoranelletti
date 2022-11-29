9eaa5ef1218a482f8926a46abf979469fd0fbf87
theoranelletti <111407220+theoranelletti@users.noreply.github.com>
2022-11-29 14:38:31 -0800

    Updating assessments/2022-11-29-numeric/code.js

--- assessments/2022-11-29-numeric/code.js
+++ assessments/2022-11-29-numeric/code.js
@@ -5,3 +5,11 @@ const totalEggs = (hard, soft) => {
 const chocolatesPerPerson = (chocolate, people) => {
   return Math.floor(chocolate / people);
 }
+
+const extraChocolates = (chocolate, people) => {
+  return 
+}
+
+const leftOut = (chocolate, people) => {
+  return Math.max(chocolate  people)
+}
