4d26545a0aae5019195a021634d4f4799643ed05
theoranelletti <111407220+theoranelletti@users.noreply.github.com>
2022-11-29 15:03:29 -0800

    Updating assessments/2022-11-29-numeric/code.js

--- assessments/2022-11-29-numeric/code.js
+++ assessments/2022-11-29-numeric/code.js
@@ -19,7 +19,7 @@ const probabilityAllHeads = (numbercoinflipped) => {
 }
 
 const futureHour = (current, future) => {
-  return Math.abs(current + (future - 12));
+  return Math.abs(current + (future % 2));
 }
 
 const presentsBudget = (friends, price) => {
