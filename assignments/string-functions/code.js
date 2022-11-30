64e03d95fea892ccd6fd35387164864b4af5cd75
theoranelletti <111407220+theoranelletti@users.noreply.github.com>
2022-11-30 15:20:36 -0800

    Updating assignments/string-functions/code.js

--- assignments/string-functions/code.js
+++ assignments/string-functions/code.js
@@ -11,5 +11,5 @@ const firstAndLast = (s) => {
 }
 
 const swapFrontAndBack = (s) => {
-  return s.substring(s.length / 2, s.length) + substring(0, s.length / 2)
+  return s.substring(s.length / 2, s.length) + s.substring(0, s.length / 2)
 }
