ca34acf85663cddcb73bfdf1069c1552a0107690
theoranelletti <111407220+theoranelletti@users.noreply.github.com>
2022-11-30 15:22:15 -0800

    Updating assignments/string-functions/code.js

--- assignments/string-functions/code.js
+++ assignments/string-functions/code.js
@@ -13,3 +13,7 @@ const firstAndLast = (s) => {
 const swapFrontAndBack = (s) => {
   return s.substring(s.length / 2, s.length) + s.substring(0, s.length / 2)
 }
+
+const simplePigLatin = (s, index) => {
+  return 
+}
