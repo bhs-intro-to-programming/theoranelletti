cf0b34e0cfa48798c282d64eb3e7b359ffb6d91c
theoranelletti <111407220+theoranelletti@users.noreply.github.com>
2022-12-05 15:00:34 -0800

    Updating assessments/2022-12-05-strings/code.js

--- assessments/2022-12-05-strings/code.js
+++ assessments/2022-12-05-strings/code.js
@@ -5,3 +5,10 @@ const upToX = (s) => {
 const pair = (s1, s2) => {
   return (s1 + ' and ') + s2
 }
+
+const containsX = (s) => {
+  if (s.substring(0, x)) {
+    return true
+  }
+  else return false
+}
