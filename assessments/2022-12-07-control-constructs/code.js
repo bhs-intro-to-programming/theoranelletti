4bec0b92d916c410c9f8caf65edbaa57d7c9341f
theoranelletti <111407220+theoranelletti@users.noreply.github.com>
2022-12-07 15:26:00 -0800

    Updating assessments/2022-12-07-control-constructs/code.js

--- assessments/2022-12-07-control-constructs/code.js
+++ assessments/2022-12-07-control-constructs/code.js
@@ -119,7 +119,7 @@ const threewayClassify = (num) => {
 const sumOfSquares = (num) => {
   let sum = 0
   for (let i = 0; i < num; i++) {
-    (((Math.floor(num)) ** 2) - num)
+    sum+= [num**2]
   }
   return sum
 }
