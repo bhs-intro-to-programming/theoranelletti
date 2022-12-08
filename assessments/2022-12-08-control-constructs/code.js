5a0a22a391d17b469b3c8898209878e43ef987a1
theoranelletti <111407220+theoranelletti@users.noreply.github.com>
2022-12-08 14:57:51 -0800

    Updating assessments/2022-12-08-control-constructs/code.js

--- assessments/2022-12-08-control-constructs/code.js
+++ assessments/2022-12-08-control-constructs/code.js
@@ -23,7 +23,7 @@ const timesTable = (num) => {
 }
 
 const containsX = (s) => {
-  if (s.indexOf(s.length, 'x')) {
+  if (s.indexOf(s.length, x)) {
     return true
   }
   else return false
@@ -31,6 +31,6 @@ const containsX = (s) => {
 
 const sumSquares = (num) => {
   for(let i = 0; i < num - 1; i++) {
-    return num
+    return num ** 2
     }
 }
