d66d07a5b1a0240c35dd8beb15000fe213da15e7
theoranelletti <111407220+theoranelletti@users.noreply.github.com>
2022-12-08 14:40:11 -0800

    Updating assessments/2022-12-08-control-constructs/code.js

--- assessments/2022-12-08-control-constructs/code.js
+++ assessments/2022-12-08-control-constructs/code.js
@@ -1,6 +1,10 @@
 const yesIfEven = (num) => {
-  if(num % 2 === 0) {
+  if (num % 2 === 0) {
     return 'yes'
   }
   else return 'no'
+}
+
+const countXs = (s) => {
+  return s.substring(0, 'x')
 }
