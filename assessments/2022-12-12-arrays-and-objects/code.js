1d7339f5d6007e17b01cd4b9cd52624c37117a4e
theoranelletti <111407220+theoranelletti@users.noreply.github.com>
2022-12-12 15:33:04 -0800

    Updating assessments/2022-12-12-arrays-and-objects/code.js

--- assessments/2022-12-12-arrays-and-objects/code.js
+++ assessments/2022-12-12-arrays-and-objects/code.js
@@ -8,13 +8,13 @@ const makeBoard = () => {
           ['', '', ''])
 }
 
+const makeMove = (mark, row, column) => {
+  return {'mark' : mark, 'row' : row, 'column' : column}
+}
+
 const allTheSame = (array) => {
   if (array[0] === array[1] && array[1] === array[2] && array[2] === array[0]) {
     return true
   }  
   else return false
-}
-
-const makeMove = (mark, row, column) => {
-  return {'mark' : mark, 'row' : row, 'column' : column}
 }
