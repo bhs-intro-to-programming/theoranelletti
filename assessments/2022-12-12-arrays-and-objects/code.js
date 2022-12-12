f96e7403196b37d7ce52c41b724564dc183a5b9a
theoranelletti <111407220+theoranelletti@users.noreply.github.com>
2022-12-12 15:28:22 -0800

    Updating assessments/2022-12-12-arrays-and-objects/code.js

--- assessments/2022-12-12-arrays-and-objects/code.js
+++ assessments/2022-12-12-arrays-and-objects/code.js
@@ -13,7 +13,7 @@ const makeMove = (mark, row, column) => {
 }
 
 const allTheSame = (array) => {
-  if (array['', '', ''] === array['','','']) {
+  if (array[0] === array[1] && array[1] === array[2] && array[2] === array[0]) {
     return true
   }  
   else return false
