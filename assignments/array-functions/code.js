a25048411a478da726ef080185fda77ec140b1f5
theoranelletti <111407220+theoranelletti@users.noreply.github.com>
2022-12-13 15:27:31 -0800

    Updating assignments/array-functions/code.js

--- assignments/array-functions/code.js
+++ assignments/array-functions/code.js
@@ -27,8 +27,8 @@ const evens = (array) => {
 
 const anyOverOneHundred = (array) => {
   let huns = 0
-  for (let i = 0; i < array.length; i++) {
-    if (array[i] > 100) {
+  for (const element of array ) {
+    if (element > 100) {
       huns = 1
     }
     if (huns === 1) {
